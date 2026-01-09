35d3a73d1dad3307c110114b6094a817fc2f6ea6
sasharity <iruogheneomaraye@gmail.com>
2026-01-09 15:25:30 +0100

    First Activity

--- server.js
+++ server.js
@@ -6,6 +6,7 @@
  * Require Statements
  *************************/
 const express = require("express")
+const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
@@ -13,7 +14,19 @@ const static = require("./routes/static")
 /* ***********************
  * Routes
  *************************/
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout") //Not at views root
+
+/* ***********************
+ * Routes
+ *************************/
 app.use(static)
+
+// Index route
+app.get("/", function (req, res) {
+  res.render("index", {title: "Home"})
+})
 
 /* ***********************
  * Local Server Information
